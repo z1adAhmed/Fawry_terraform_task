d84a24263eb516c4df9d4166d8954d60395ce3dd
Ziad Ahmed <ziad.a200343@gmail.com>
2025-06-28 15:33:44 +0300

    Test 2

--- envs/pre-prod/backend.tf
+++ envs/pre-prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "poc-tf-state-ziad"
+    bucket         = "poc-prod-vpc-logs-709efebc"
     key            = "pre-prod/terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-locks"
