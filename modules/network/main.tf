717883f52f03e06e785aca4cc453670aee72cd4d
Ziad Ahmed <ziad.a200343@gmail.com>
2025-07-06 17:04:32 +0300

    Add port 6443 to security group for K3s API server

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -66,6 +66,12 @@ resource "aws_security_group" "k3s" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port   = 30000
     to_port     = 32767
     protocol    = "tcp"
